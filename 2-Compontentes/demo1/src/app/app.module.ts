8a669b4d9ac0147a6b33a3010e8e7ad176cde545
Laritas trabajo <axeellara27@gmail.com>
2022-09-20 13:32:21 -0500

    componentes

--- 2-Compontentes/demo1/src/app/app.module.ts
+++ 2-Compontentes/demo1/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ListaComponent } from './components/lista/lista.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentComponent } from './components/student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ListaComponent
+    ListaComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
